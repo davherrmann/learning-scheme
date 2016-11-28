6efc9d9f6e388c0848dd1404537a7ef58221572e
David Herrmann <davherrmann@googlemail.com>
2016-11-28 02:42:21 +0100

    add exercise 3-2-1 with usage examples

---
+++ exercises/3-2-1.scm
@@ -0,0 +1,20 @@
+#lang racket
+
+; differentiate : function number -> function
+; finds the derivative of a function f for a small k > 0
+(define (differentiate f k)
+  (lambda (x)
+    (/ (- (f (+ x k))
+          (f (- x k)))
+       (* 2 k))))
+
+
+
+(define (ableiten fkt k) (differentiate fkt k))
+
+(define (sqr x) (* x x))
+(define ablsqr (ableiten sqr 0.1))
+ablsqr
+(ablsqr 1)
+(ablsqr 2)
+(ablsqr 3)
