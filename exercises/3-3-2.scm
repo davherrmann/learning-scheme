52d6925c5e5c54fba9ae275f6b2fedb8545369e3
David Herrmann <davherrmann@googlemail.com>
2016-11-28 04:56:42 +0100

    implement missing functions for message dispatching based associative list

--- exercises/3-3-2.scm
+++ exercises/3-3-2.scm
@@ -1,12 +1,44 @@
 #lang racket
 
+; as-find : pair list -> list
+; gives a list of values of all pairs in l with the key s
+(define (as-find s l)
+  (if (null? l)
+      '()
+      (if (eq? (caar l) s)
+          (cons (cadar l) (as-find s (cdr l)))
+          (as-find s (cdr l)))))
+
+; creates a message dispatcher for the given associative list
 (define (make-as as)
   (define (dispatch m d)
-      (cond ((eq? m 'null?) (null? as))
-            (else (error "no handler for message " m))))
+    (let ((f? (lambda (f) (eq? m f))))
+      (cond ((f? 'null?) (null? as))
+            ((f? 'car) (if (null? as)
+                           '()
+                           (car as)))
+            ((f? 'cdr) (if (null? as)
+                           '()
+                           (make-as (cdr as))))
+            ((f? 'cons) (if (pair? d)
+                            (make-as (cons d as))
+                            (error "given argument is not a valid (key value) pair")))
+            ((f? 'find) (as-find d as))
+            (else (error "no handler for message " m)))))
   dispatch)
 
+; creates a message dispatcher for an empty associative list
 (define (make-empty-as) (make-as '()))
 
 (define a1 (make-empty-as))
-(a1 'null? 'dummy)
+(define a2 (a1 'cons '(b 2)))
+(define a3 (a2 'cons '(d 4)))
+(define a4 (a3 'cons '(c 5)))
+(define a5 (a4 'cons '(a 6)))
+(define a6 (a5 'cons '(c 4)))
+(define a7 (a6 'cons '(b 7)))
+(define a8 (a7 'cons '(a 1)))
+(a8 'find 'b)
+(a8 'find 'a)
+(a7 'find 'a)
+((a8 'cdr 'dummy) 'find 'a)
